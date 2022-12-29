183f03955187045908a43565c0950ddfa485c2ca
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2022-12-30 02:28:09 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Beginner - Reduce but Grow.c
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// 
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// -----------------------------------------------------------
+
+#include <stddef.h>
+
+int grow(size_t size, const int arr[size]){
+  int answer = 1;
+  for (size_t i = 0; i < size; i++){
+    answer *= arr[i];
+  }
+  return answer;
+}
