e1ad36eabefeb1c386b88a411f572903e4477608
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-02-09 22:47:48 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Geometry Basics Distance between points in 2D.c
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------
+// This series of katas will introduce you to basics of doing geometry with computers.
+// 
+// Point objects have attributes x and y.
+// 
+// Write a function calculating distance between Point a and Point b.
+// 
+// Tests compare expected result and actual answer with tolerance of 1e-6.
+// -----------------------------------------------------------
+
+#include <math.h>
+
+typedef struct Point{
+    double x;
+    double y;
+} point;
+
+double distance_between_points(point a, point b){
+  return hypot(a.x - b.x, a.y - b.y);
+}
