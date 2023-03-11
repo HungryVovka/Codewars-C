3e51f64c7f2e7a16decbcbe6b19fa91cb23e052f
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-11 23:55:56 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Function 2 - squaring an argument.c
@@ -0,0 +1,10 @@
+// -----------------------------------------------------------
+// Now you have to write a function that takes an argument and returns the square of it.
+// -----------------------------------------------------------
+
+#include <stdint.h>
+#include <math.h>
+
+int32_t square(int32_t number){
+  return pow(number, 2);
+}
