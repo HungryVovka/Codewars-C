b754fce506a0dd9e9cb85592b3ad2bdac979d096
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-06 00:57:38 +0500

    feat: 8 kyu solutions

--- 8 kyu/Convert a Boolean to a String.c
+++ 8 kyu/Convert a Boolean to a String.c
@@ -11,3 +11,24 @@ const char *boolean_to_string(bool b)
 {
     return b == true ? "true" : "false";
 }
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2022 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-C
+// (https://github.com/HungryVovka/Codewars-C)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-C/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
--- 8 kyu/Price of Mangoes.c
+++ 8 kyu/Price of Mangoes.c
@@ -15,3 +15,24 @@ unsigned mango(unsigned quantity, unsigned price)
     int discount = trunc(quantity / 3) * price;
     return quantity * price - discount;
 }
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2022 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-C
+// (https://github.com/HungryVovka/Codewars-C)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-C/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
