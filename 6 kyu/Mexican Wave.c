7bb74aec86bf5a00b970ff5b67bdf649c39971d0
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-06 00:50:01 +0500

    feat: 6 kyu solutions

--- 6 kyu/Mexican Wave.c
+++ 6 kyu/Mexican Wave.c
@@ -64,6 +64,6 @@ void wave(const char *y, char **target)
 // License is GNU General Public License v3.0
 // (https://github.com/HungryVovka/Codewars-C/blob/main/LICENSE)
 // 
-// You should have received a copy of the GNU Lesser Public License
+// You should have received a copy of the GNU General Public License v3.0
 // along with this code. If not, see http://www.gnu.org/licenses/
 // -----------------------------------------------------------
--- 6 kyu/Next smaller pronic.c
+++ 6 kyu/Next smaller pronic.c
@@ -42,6 +42,6 @@ unsigned long long next_smaller_pronic(unsigned long long n)
 // License is GNU General Public License v3.0
 // (https://github.com/HungryVovka/Codewars-C/blob/main/LICENSE)
 // 
-// You should have received a copy of the GNU Lesser Public License
+// You should have received a copy of the GNU General Public License v3.0
 // along with this code. If not, see http://www.gnu.org/licenses/
 // -----------------------------------------------------------
