238276d4992aff53a7bf0abd05005b4f9a91ecde
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-11-18 21:41:40 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/MakeUpperCase.c
@@ -0,0 +1,36 @@
+// -----------------------------------------------------------
+// Write a function which converts the input string to uppercase.
+// -----------------------------------------------------------
+
+char *makeUpperCase (char *string)
+{
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] -= 32;
+		}
+	}
+	return string;
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2022 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-C
+// (https://github.com/HungryVovka/Codewars-C)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-C/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
